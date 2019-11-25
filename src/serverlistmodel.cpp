0c27314978707c58775e11a52d8b12ecb3f5b044
Benjamin O <jurplel@interversehq.com>
2019-11-25 12:11:14 -0500

    i see

--- src/serverlistmodel.cpp
+++ src/serverlistmodel.cpp
@@ -42,7 +42,7 @@ QHash<int, QByteArray> ServerListModel::roleNames() const {
 void ServerListModel::refresh()
 {
     serverList.clear();
-    serverList.append(QStringList({"Local Server", "0.0.0.0:32019"}));
+    serverList.append(Server({"Local Server", "0.0.0.0:32019"}));
 
     QString retrievedServers = qPrintable(synchro_get_server_list(""));
     QStringList retrievedServerList = retrievedServers.split(";");
@@ -50,6 +50,6 @@ void ServerListModel::refresh()
     foreach (auto serverString, retrievedServerList) {
         QStringList serverFields = serverString.split(",");
 
-        serverList.append(QStringList({serverFields[0], serverFields[1]}));
+        serverList.append(Server({serverFields[0], serverFields[1]}));
     }
 }
